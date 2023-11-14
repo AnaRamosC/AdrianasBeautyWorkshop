24e10bcd93a051a02d3b74cb6c9e5f30ab65777a
AnaRamosC <anarmcs179@gmail.com>
2023-11-14 22:51:59 +0100

    mitad responsive movil hecho

--- WebSalonBelleza/avisotablets.js
+++ WebSalonBelleza/avisotablets.js
@@ -1,7 +1,25 @@
 window.onload = function() {
-    var esTablet = (window.innerWidth > 768 && window.innerWidth < 2000); // Modifica estos valores según tus necesidades
+    var esTablet = (window.innerWidth > 768 && window.innerWidth < 1024); // Modifica estos valores según tus necesidades
 
     if (esTablet && (navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/tablet/i))) {
-        alert("Si estás visitándonos desde un iPad o tablet, te aconsejamos que pongas tu dispositivo en posición horizontal para una mejor experiencia.");
+        alert("Si estás visitándonos desde un iPad o tablet, por favor pon tu dispositivo en posición horizontal para una mejor experiencia.");
     }
 }
+
+
+window.addEventListener('load', function() {
+    function mostrarAlerta() {
+        alert("Si estás visitándonos desde un iPad o tablet, por favor pon tu dispositivo en posición horizontal para una mejor experiencia.");
+    }
+
+    function verificarOrientacion() {
+        var esTablet = (window.innerWidth > 768 && window.innerWidth < 1024);
+
+        if (esTablet && (navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/tablet/i))) {
+            mostrarAlerta();
+        }
+
+
+    }
+    
+})
