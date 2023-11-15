877ffe6348ce65f8ece6e4558be5937fd992c0ee
AnaRamosC <anarmcs179@gmail.com>
2023-11-15 19:09:21 +0100

    prueba ios

--- WebSalonBelleza/Resultados/tactil.js
+++ WebSalonBelleza/Resultados/tactil.js
@@ -1,20 +1,76 @@
+// Para dispositivos táctiles (iOS y Android)
 document.querySelectorAll('.conjunto>div>div').forEach(function (element) {
+    // Variable para rastrear el estado del toque
+    let isTouchActive = false;
+  
+    // Función para aplicar el efecto de escala
+    function applyScaleEffect() {
+      element.style.transform = 'scale(1.8)';
+    }
+  
+    // Función para restaurar el tamaño original
+    function resetScaleEffect() {
+      element.style.transform = 'scale(1)';
+    }
+  
+    // Evento de inicio del toque
     element.addEventListener('touchstart', function () {
-      this.style.transform = 'scale(1.8)';
+      isTouchActive = true;
+      applyScaleEffect();
     });
   
+    // Evento de fin del toque
     element.addEventListener('touchend', function () {
-      this.style.transform = 'scale(1)';
+      if (isTouchActive) {
+        isTouchActive = false;
+        resetScaleEffect();
+      }
+    });
+  
+    // Evento de cancelación del toque (puede ocurrir en ciertos escenarios)
+    element.addEventListener('touchcancel', function () {
+      if (isTouchActive) {
+        isTouchActive = false;
+        resetScaleEffect();
+      }
     });
   });
 
 
-  document.querySelectorAll('.conjunto>div>div h3').forEach(function (element) {
+  // Para dispositivos táctiles (iOS y Android)
+document.querySelectorAll('.conjunto>div>div h3').forEach(function (element) {
+    // Variable para rastrear el estado del toque
+    let isTouchActive = false;
+  
+    // Función para aplicar el efecto de escala
+    function applyScaleEffect() {
+      element.style.transform = 'scale(1.8)';
+    }
+  
+    // Función para restaurar el tamaño original
+    function resetScaleEffect() {
+      element.style.transform = 'scale(1)';
+    }
+  
+    // Evento de inicio del toque
     element.addEventListener('touchstart', function () {
-      this.style.transform = 'scale(1.8)';
+      isTouchActive = true;
+      applyScaleEffect();
     });
   
+    // Evento de fin del toque
     element.addEventListener('touchend', function () {
-      this.style.transform = 'scale(1)';
+      if (isTouchActive) {
+        isTouchActive = false;
+        resetScaleEffect();
+      }
+    });
+  
+    // Evento de cancelación del toque (puede ocurrir en ciertos escenarios)
+    element.addEventListener('touchcancel', function () {
+      if (isTouchActive) {
+        isTouchActive = false;
+        resetScaleEffect();
+      }
     });
   });
